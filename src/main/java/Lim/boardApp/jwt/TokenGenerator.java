0a1842c52c473f0dad6087bc0bcea7f2738f850d
hyunwoo0318 <hyunwoo0318@naver.com>
2023-02-04 12:49:50 +0900

    tokenGenerator 구현

--- src/main/java/Lim/boardApp/jwt/TokenGenerator.java
+++ src/main/java/Lim/boardApp/jwt/TokenGenerator.java
@@ -1,6 +1,8 @@
 package Lim.boardApp.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
@@ -17,21 +19,41 @@ import java.util.stream.Collectors;
 
 @Component
 public class TokenGenerator {
-    /*private byte[] key;
-    private Date accessTokenExpire;
-    private Date refreshTokenExpire;
+    private Key key;
+    private Date tokenExpire;
 
 
     public TokenGenerator(@Value("${jwt.secret}") String secretKey){
-        key = Base64Utils.decodeFromString(secretKey);
+        byte[] bytes = Base64Utils.decodeFromString(secretKey);
+        this.key = Keys.hmacShaKeyFor(bytes);
     }
 
-    public TokenDto generateToken(Authentication authentication) {
-        authentication.getAuthorities().stream()
+    public TokenDto generateToken(Authentication authentication, String id) {
+        String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
 
         long now = new Date().getTime();
-        accessTokenExpire = new Date(now + JwtConst.ACCESS_TOKEN_EXPIRED_TIME);
-    }*/
+        tokenExpire = new Date(now + JwtConst.TOKEN_EXPIRED_TIME);
+
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities) // claim -> jwt body custom claim
+                .setAudience(id)
+                .setIssuer(JwtConst.TOKEN_ISSUER)
+                .setExpiration(tokenExpire) // 일반적인 claim에 대해서는 setter를 제공함
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        String refreshToken = Jwts.builder()
+                .setExpiration(tokenExpire) // 표준에서 refresh token과 access token을 유효기간을 같이 설정하도록 권고
+                .signWith(key,SignatureAlgorithm.HS256)
+                .compact();
+
+        return TokenDto.builder()
+                .type("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
